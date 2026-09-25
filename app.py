e395072ed09765473109399ed02aef73afc74d8d
tridip2gogoi <tridipgogoi2802@gmail.com>
2026-09-25 12:22:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -140,7 +140,7 @@ USERS = {
     # Admin Accounts
     "tridip.gogoi": {"password": "Gogoi@6095", "role": "Admin", "name": "Super Admin (All JC)", "jc": "All"},
     "sukanta.biswas": {"password": "Biswas@2026", "role": "Admin", "name": "(Central Admin)", "jc": "All"},
-    "rintu.tamuli": {"password": "Tamuli@2026", "role": "Admin", "name": "(Central Admin)", "jc": "All"},
+    "rintu.tamuli": {"password": "Tamuli@2026", "role": "Admin", "name": "(Central Manager)", "jc": "All"},
 
     # ----------------- UTILITY TECHNICIANS (64 USERS MAPPED TO JC) -----------------
     # Shillong JC
